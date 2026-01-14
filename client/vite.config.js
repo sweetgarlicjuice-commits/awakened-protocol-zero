b459c0415020159187fc1460c6ac37ee76f28b87
fauziashraf <132036977+fauziashraf@users.noreply.github.com>
2026-01-15 05:20:28 +0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
+  build: {
+    outDir: 'dist',
+    copyPublicDir: true
   }
-});
+})
