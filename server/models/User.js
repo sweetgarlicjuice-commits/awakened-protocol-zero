daf2485327c455a221690034d8afcab8d60ba73d
fauziashraf <132036977+fauziashraf@users.noreply.github.com>
2026-01-16 14:07:00 +0800

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -6,9 +6,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    lowercase: true,
+    trim: true,
     minlength: 3,
-    maxlength: 20,
-    trim: true
+    maxlength: 20
   },
   password: {
     type: String,
@@ -26,27 +27,35 @@ const userSchema = new mongoose.Schema({
   },
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'User',
+    default: null
+  },
+  lastLogin: {
+    type: Date,
+    default: null
   },
   createdAt: {
     type: Date,
     default: Date.now
-  },
-  lastLogin: {
-    type: Date
   }
 });
 
 // Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 // Compare password method
 userSchema.methods.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
 export default mongoose.model('User', userSchema);
